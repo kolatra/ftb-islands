c067d2e911fb88172bf52d468ed4ef082dcea197
Tyler <t.louks88@gmail.com>
2016-12-02 23:17:49 -0700

    - Version 1.3.2

--- src/main/java/com/cricketcraft/ftbisland/FTBIslands.java
+++ src/main/java/com/cricketcraft/ftbisland/FTBIslands.java
@@ -37,7 +37,7 @@ import net.minecraftforge.common.config.Configuration;
 public class FTBIslands {
     public static final String MODID = "FTBI";
     public static final String NAME = "FTB Islands";
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
     public static int maxIslands;
     public static File islands;
     public static Logger logger;
